53d0f7e4be7e82d57363780470b0eb14ca4cc8d9
Eknoor-singh01 <eknoorsingh.engineer@gmail.com>
2026-06-21 22:25:48 +0530

    Add hello.py with a simple greeting program

---
+++ Level  1  -  hello.py/hello.py
@@ -0,0 +1,8 @@
+# My First Python Program
+# Goal: Dual Studium Germany 🇩🇪
+# Started: June 2026 
+
+name = input("What is your name? ")
+# Using an f-string (modern Python) makes joining text easier!
+print(f"Hello {name}!") 
+print("Welcome to my Python journey!")
